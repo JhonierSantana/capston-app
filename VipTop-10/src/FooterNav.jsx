e91cd11efeba9dbfdc3d17aa7f5feded546cf3fc
Jhonier Yesid Santana Pedroza <santana@DESKTOP-ISQ43BP>
2023-06-10 11:06:29 -0500

    upgrade

--- VipTop-10/src/FooterNav.jsx
+++ VipTop-10/src/FooterNav.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
-import './App.css'
+import Container from 'react-bootstrap/Container';
+import './App.css';
 
 export const FooterNav = () => {
   const alertClicked = () => {
@@ -8,23 +9,21 @@ export const FooterNav = () => {
   };
 
   return (
-    <>
-      <footer className='footer-bar py-2'>
-        <Container>
-          <p className='page-info col-md-6 fs-5'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Nesciunt quibusdam aut, laborum voluptate aspernatur repellat
-            labore sunt eos unde, odio ducimus reiciendis error consequuntur porro!
-            Temporibus beatae quaerat error est?
-          </p>
-          <ListGroup variant="flush">
-            <ListGroup.Item action onClick={alertClicked}>Link 1</ListGroup.Item>
-            <ListGroup.Item action onClick={alertClicked}>Link 2</ListGroup.Item>
-            <ListGroup.Item action onClick={alertClicked}>Link 3</ListGroup.Item>
-            <ListGroup.Item action onClick={alertClicked}>Link 4</ListGroup.Item>
-          </ListGroup>
-        </Container>
-      </footer>
-    </>
-  )
-}
+    <footer className='footer-bar py-2'>
+      <Container>
+        <p className='page-info col-md-6 fs-5'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Nesciunt quibusdam aut, laborum voluptate aspernatur repellat
+          labore sunt eos unde, odio ducimus reiciendis error consequuntur porro!
+          Temporibus beatae quaerat error est?
+        </p>
+        <ListGroup variant="flush">
+          <ListGroup.Item action onClick={alertClicked}>Link 1</ListGroup.Item>
+          <ListGroup.Item action onClick={alertClicked}>Link 2</ListGroup.Item>
+          <ListGroup.Item action onClick={alertClicked}>Link 3</ListGroup.Item>
+          <ListGroup.Item action onClick={alertClicked}>Link 4</ListGroup.Item>
+        </ListGroup>
+      </Container>
+    </footer>
+  );
+};
--- VipTop-10/src/main.jsx
+++ VipTop-10/src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-import { FooterNav } from './FooterNav.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App.jsx';
+import { FooterNav } from './FooterNav.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <>
-  <App />
-  </>
-)
+    <App />
+  </>,
+  document.getElementById('root')
+);
 
-ReactDOM.createRoot(document.getElementById('footer-navbar')).render(
+ReactDOM.render(
   <>
-  <FooterNav />
-  </>
-)
+    <FooterNav />
+  </>,
+  document.getElementById('footer-navbar')
+);
