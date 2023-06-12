794093af4f37cc0c593644ef8b1b1c8e55055665
Dannyelgeek <daot1993@gmail.com>
2023-06-12 09:06:40 -0500

    adding search bar #2

--- VipTop-10/src/components/SearchBar.jsx
+++ VipTop-10/src/components/SearchBar.jsx
@@ -3,13 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 
 export const SearchBar = () => {
-  return (
-    <Container>
-    <div className='search-bar rounded-3 py-3'>
-        <Form>
-            <Form.Control className='mx-3' type='text' placeholder='Country...'/>
-        </Form>
-    </div>
-    </Container>
-  )
+    return (
+        <Container>
+            <div className='search-bar rounded-3 py-3'>
+                <Form>
+                    <Form.Control className='mx-3' type='text' placeholder='Country...' />
+                </Form>
+                <Form>
+                    <Form.Control className='mx-3' type='text' placeholder='City...' />
+                </Form>
+                <Form>
+                    <Form.Select className='me-3' name='City'>
+                        <option>Category...</option>
+                    </Form.Select>
+                </Form>
+            </div>
+        </Container>
+    )
 }
