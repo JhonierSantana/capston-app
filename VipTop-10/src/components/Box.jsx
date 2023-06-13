5272a6b9d7fbd7d4b22a88d88aeb9012cfc59e42
ZapataCamilo <zapatacamilo86@gmail.com>
2023-06-13 09:39:52 -0500

    box-css

--- VipTop-10/src/components/Box.jsx
+++ VipTop-10/src/components/Box.jsx
@@ -5,30 +5,30 @@ import '../style/Box.css'
 
 export const Box_content = () => {
     return (
-        <div id="myCarousel" class="carousel slide">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container d-flex">
-                        <div class="item-box rounded">Box 1</div>
-                        <div class="item-box rounded">Box 2</div>
-                        <div class="item-box rounded">Box 3</div>
+        <div id="myCarousel" className="carousel slide">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <div className="container d-flex" id='cr'>
+                        <div className="item-box rounded">Box 1</div>
+                        <div className="item-box rounded">Box 2</div>
+                        <div className="item-box rounded">Box 3</div>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <div class="container d-flex">
-                        <div class="item-box rounded">Box 4</div>
-                        <div class="item-box rounded">Box 5</div>
-                        <div class="item-box rounded">Box 6</div>
+                <div className="carousel-item">
+                    <div className="container d-flex" id='cr'>
+                        <div className="item-box rounded">Box 4</div>
+                        <div className="item-box rounded">Box 5</div>
+                        <div className="item-box rounded">Box 6</div>
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     )
