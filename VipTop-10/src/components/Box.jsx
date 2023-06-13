10356da6ebd68f17071efa10e3183f45331127f1
YeosCRN <jeoscrn20@outlook.com>
2023-06-13 15:57:00 -0500

    fix

--- VipTop-10/src/components/Box.jsx
+++ VipTop-10/src/components/Box.jsx
@@ -15,9 +15,7 @@ export const Box_content = () => {
     return (
         <div id="myCarousel" className="carousel slide">
             <Container>
-                ReactDOM.render(
-                    <Shopping />
-                )
+                <Shopping />
             </Container>
             <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
