d604b90fb81d4acddef460b85dc2a16c1749187d
YeosCRN <jeoscrn20@outlook.com>
2023-06-13 14:19:29 -0500

    Category_Contents

--- VipTop-10/src/components/Box.jsx
+++ VipTop-10/src/components/Box.jsx
@@ -3,83 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../style/App.css'
 import '../style/Box.css'
+import { Entertaiment } from './Entertaiment';
 
 export const Box_content = () => {
     return (
         <div id="myCarousel" className="carousel slide">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div className="container" id='cr'>
-                        <div className="item-box mt-3 rounded">Box 1</div>
-                        <div className="item-box mt-3 rounded">Box 2</div>
-                        <div className="item-box mt-3 rounded">Box 3</div>
-                        <div className="item-box mt-3 rounded">Box 4</div>
-                        <div className="item-box mt-3 rounded">Box 5</div>
-                        <div className="item-box mt-3 rounded">Box 6</div>
-                        <div className="item-box mt-3 rounded">Box 7</div>
-                        <div className="item-box mt-3 rounded">Box 8</div>
-                    </div >
-                </div >
-                <div className="carousel-item">
-                    <div className="container" id='cr'>
-                        <div className="item-box mt-3 rounded">Box 1</div>
-                        <div className="item-box mt-3 rounded">Box 2</div>
-                        <div className="item-box mt-3 rounded">Box 3</div>
-                        <div className="item-box mt-3 rounded">Box 4</div>
-                        <div className="item-box mt-3 rounded">Box 5</div>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="container" id='cr'>
-                        <div className="item-box mt-3 rounded">Box 1</div>
-                        <div className="item-box mt-3 rounded">Box 2</div>
-                        <div className="item-box mt-3 rounded">Box 3</div>
-                        <div className="item-box mt-3 rounded">Box 4</div>
-                        <div className="item-box mt-3 rounded">Box 5</div>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="container" id='cr'>
-                        <div className="item-box mt-3 rounded">Box 1</div>
-                        <div className="item-box mt-3 rounded">Box 2</div>
-                        <div className="item-box mt-3 rounded">Box 3</div>
-                        <div className="item-box mt-3 rounded">Box 4</div>
-                        <div className="item-box mt-3 rounded">Box 5</div>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="container" id='cr'>
-                        <div className="item-box mt-3 rounded">
-                            <picture>
-                                <h3>Car Rent</h3>
-                                <hr />
-                                <img src="../../img/img_service/car-rent.png" alt="car-rent-logo" id='logito'/>
-                            </picture>
-                        </div>
-                        <div className="item-box mt-3 rounded">
-                            <picture>
-                                <h3>Pharmacy</h3>
-                                <hr />
-                                <img src="../../img/img_service/meds.png" alt="meds-logo" />
-                            </picture>
-                        </div>
-                        <div className="item-box mt-3 rounded">
-                            <picture>
-                                <h3>Vet Med</h3>
-                                <hr />
-                                <img src="../../img/img_service/vet.png" alt="vet-logo" />
-                            </picture>
-                        </div>
-                        <div className="item-box mt-3 rounded">
-                            <picture>
-                                <h3>Gym</h3>
-                                <hr />
-                                <img src="../../img/img_service/weights.png" alt="Gym-logo" />
-                            </picture>
-                        </div>
-                    </div>
-                </div>
-            </div >
+            ReactDOM.render{
+                <Entertaiment />
+            }
             <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
---
+++ VipTop-10/src/components/Entertaiment.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../style/App.css'
+import '../style/Box.css'
+
+export const Entertaiment = () => {
+    return (
+        <div className="carousel-inner">
+            <div className="carousel-item active">
+                <div className="container" id='cr'>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Amusement Park</h3>
+                            <hr />
+                            <img src="../../img/img_service/car-rent.png" alt="Amusement Park-logo" id='logito' />
+                        </picture>
+                    </div>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Bowling Alley</h3>
+                            <hr />
+                            <img src="../../img/img_service/meds.png" alt="Bowling Alley-logo" />
+                        </picture>
+                    </div>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Casino</h3>
+                            <hr />
+                            <img src="../../img/img_service/vet.png" alt="Casino-logo" />
+                        </picture>
+                    </div>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Movie Theater</h3>
+                            <hr />
+                            <img src="../../img/img_service/weights.png" alt="Movie Theater-logo" />
+                        </picture>
+                    </div>
+                </div>
+            </div>
+            <div className="carousel-item active">
+                <div className="container" id='cr'>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Night Club</h3>
+                            <hr />
+                            <img src="../../img/img_service/car-rent.png" alt="Night Club-logo" id='logito' />
+                        </picture>
+                    </div>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Stadium</h3>
+                            <hr />
+                            <img src="../../img/img_service/meds.png" alt="Stadium-logo" />
+                        </picture>
+                    </div>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Beauty Salon</h3>
+                            <hr />
+                            <img src="../../img/img_service/vet.png" alt="Beauty Salon-logo" />
+                        </picture>
+                    </div>
+                    <div className="item-box mt-3 rounded">
+                        <picture>
+                            <h3>Spa</h3>
+                            <hr />
+                            <img src="../../img/img_service/weights.png" alt="Spa" />
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
