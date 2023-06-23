4915b5c28770065881db17b6c08bae3a32eeab93
Jhonier Yesid Santana Pedroza <santana@DESKTOP-ISQ43BP>
2023-06-23 11:24:23 -0500

    upgrade

--- VipTop-10/src/components/ListItem.jsx
+++ VipTop-10/src/components/ListItem.jsx
@@ -11,17 +11,14 @@ export const ListItem = ({ top_image, top_place_img }) => {
       <Row>
         <Col xs={10} sm={8} md={6} lg={4} className="text-center">
           <div className='list1 rounded-4 my-3'>
-            <div className="image-container">
-              <img className='top_img' src={top_image} alt="Top image" />
-            </div>
-            <div className="content">
-              <h4>Holbertonlandia</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus accusantium dolore perspiciatis, modi illum animi eum, recusandae nesciunt est voluptatibus facere? Aliquid a blanditiis porro facere! Corrupti nulla consectetur provident.</p>
-            </div>
+            <img className='top_img' src={`../../img/img_top/${top_image}`} alt="Top image" />
+            <h4>Holbertonlandia</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus accusantium dolore perspiciatis, modi illum animi eum, recusandae nesciunt est voluptatibus facere? Aliquid a blanditiis porro facere! Corrupti nulla consectetur provident.</p>
           </div>
         </Col>
       </Row>
     </Container>
-  );
+  )
 };
+
 
