bce437fbe4b068fe38e0b599fc7f1bcf6c213df0
ZapataCamilo <zapatacamilo86@gmail.com>
2023-06-28 09:27:46 -0500

    Top-Casino

--- VipTop-10/src/routes/Entertainment/CasinoTop.jsx
+++ VipTop-10/src/routes/Entertainment/CasinoTop.jsx
@@ -12,19 +12,39 @@ export const CasinoTop = () => {
   return (
     <>
     <Headerbar />
-    <h1 className='title mt-3'>Top's title</h1>
+    <h1 className='title mt-3'>Top 10 casinos</h1>
     <hr />
     <Container className='Outer'>
-        <ListItem className='list1' top_image='top_10.png' />
-        <ListItem className='list1' top_image='top_9.png' />
-        <ListItem className='list1' top_image='top_8.png' />
-        <ListItem className='list1' top_image='top_7.png' />
-        <ListItem className='list1' top_image='top_6.png' />
-        <ListItem className='list1' top_image='top_5.png' />
-        <ListItem className='list1' top_image='top_4.png' />
-        <ListItem className='list1' top_image='top_3.png' />
-        <ListItem className='list1' top_image='top_2.png' />
-        <ListItem className='list1' top_image='top_1.png' />
+        <ListItem className='list1' top_image='top_10.png' place_img='casino_1.jpg'
+        place_title='Diamond Sky Casino' place_description='Fun, bet, night.'
+        place_address='1313 Hiney Road, Nevada, 702-245-5532' />
+        <ListItem className='list1' top_image='top_9.png' place_img='casino_1.jpg'
+        place_title='Sea of gold Casino' place_description='Fun, bet, night.'
+        place_address='3710 Hall Street, Nevada, 702-594-0623' />
+        <ListItem className='list1' top_image='top_8.png' place_img='casino_1.jpg'
+        place_title="Fortune's Edge Casino" place_description='Fun, bet, night.'
+        place_address='3676 Sunrise Road, Nevada, 702-884-9835' />
+        <ListItem className='list1' top_image='top_7.png' place_img='casino_1.jpg'
+        place_title='Starlight Oasis Casino' place_description='Fun, bet, night.'
+        place_address='33 Hall Street, Nevada, 702-438-8461' />
+        <ListItem className='list1' top_image='top_6.png' place_img='casino_1.jpg'
+        place_title='Midnight Mirage Casino' place_description='Fun, bet, night.'
+        place_address='786 Hickory Ridge Drive, Nevada, 702-635-4704' />
+        <ListItem className='list1' top_image='top_5.png' place_img='casino_1.jpg'
+        place_title='Royal Flush Casino' place_description='Fun, bet, night.'
+        place_address='316 Mesa Drive, Nevada, 702-393-5825' />
+        <ListItem className='list1' top_image='top_4.png' place_img='casino_1.jpg'
+        place_title='Emerald Isle Casino' place_description='Fun, bet, night.'
+        place_address='2644 Mesa Drive, Nevada, 702-407-5828' />
+        <ListItem className='list1' top_image='top_3.png' place_img='casino_1.jpg'
+        place_title='Silver Sands Casino' place_description='Fun, bet, night.'
+        place_address='2115 Hall Street, Nevada, 702-554-9073' />
+        <ListItem className='list1' top_image='top_2.png' place_img='casino_1.jpg'
+        place_title='Golden Lion Casino' place_description='Fun, bet, night.'
+        place_address='4443 Hall Street, Nevada, 702-497-9610' />
+        <ListItem className='list1' top_image='top_1.png' place_img='casino_1.jpg'
+        place_title='Platinum Peak Casino' place_description='Fun, bet, night.'
+        place_address='964 Platinum Drive, Nevada, 724-769-9720724-769-9720' />
       </Container>
     <FooterNav />
     </>
