7ba514c060fd7bad9027c376a9c409b5da1ec80b
Jhonier Yesid Santana Pedroza <santana@DESKTOP-ISQ43BP>
2023-06-29 09:09:15 -0500

    footer update

--- VipTop-10/src/components/FooterNav.jsx
+++ VipTop-10/src/components/FooterNav.jsx
@@ -1,18 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export const FooterNav = () => {
-
   const description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At architecto possimus tenetur iure ducimus eveniet deserunt sapiente! Fugiat nihil id aperiam laboriosam magnam reiciendis, facilis officiis, possimus ad, veniam inventore?";
-  const title = "Titulo del Footer";
+  const title = <img className='img-footer' src="../img/logo-no-background.png" alt="logo" />;
 
   const columns = [
-    {
-      title: "Sponsors",
-      resources: [
-        { name: "Holberton", link: "/item1" },
-        { name: "Laboratio", link: "/item2" }
-      ]
-    },
     {
       title: "Front-end Team",
       resources: [
@@ -29,24 +21,40 @@ export const FooterNav = () => {
         { name: "Esteban Enriquez", link: "/item10" },
         { name: "Christian Jurado", link: "/item11" }
       ]
+    },
+    {
+      title: "Sponsors",
+      resources: [
+        { name: "Holberton", link: "/item1", image: "holberton.png" },
+        { name: "Laboratio", link: "/item2", image: "laboratio.png" }
+      ]
     }
   ];
 
   return (
     <footer className="footer">
-      <div className="container">
+      <div className="container1">
         <div className="footer-description">
           <h3>{title}</h3>
-          <p>{description}</p>
         </div>
         <div className="footer-columns">
           {columns.map((column, index) => (
-            <div key={index} className="footer-column">
+            <div key={index} className={`footer-column ${column.title === "Sponsors" ? "right-column" : ""}`}>
               <h4>{column.title}</h4>
               <ul className="footer-resources">
                 {column.resources.map((resource, index) => (
                   <li key={index}>
-                    <a href={resource.link}>{resource.name}</a>
+                    <a href={resource.link}>
+                      {resource.name === "Holberton" && (
+                        <img className='img-sponsors' src="../../img/icons/holberton.jpg" alt="logo" />
+                      )}
+                      {resource.name === "Laboratio" && (
+                        <img className='img-sponsors1' src="../../img/icons/laboratio.jpeg" alt="logo" />
+                      )}
+                      {resource.name !== "Holberton" && resource.name !== "Laboratio" && (
+                        <span>{resource.name}</span>
+                      )}
+                    </a>
                   </li>
                 ))}
               </ul>
